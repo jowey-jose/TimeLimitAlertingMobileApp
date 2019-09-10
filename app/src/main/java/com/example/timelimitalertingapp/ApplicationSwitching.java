30dbc1405b0445a78ec46ee5f6b6c718f860b54a
jowey-jose <josephmbuku17@gmail.com>
2019-09-10 10:27:06 +0300

    Login, Registration and Tabbed Activity

---
+++ app/src/main/java/com/example/timelimitalertingapp/ApplicationSwitching.java
@@ -0,0 +1,26 @@
+package com.example.timelimitalertingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ApplicationSwitching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_switching);
+
+
+    }
+
+
+
+
+}
